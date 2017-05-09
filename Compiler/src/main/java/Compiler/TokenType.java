8a8b7e4d8aa4355664d1ca510301d3a6495147cf
Alexandros Kritikos <akritikos@outlook.com>
2017-05-09 19:07:39 +0300

    Capturing TokenTypes with regex

--- Compiler/src/main/java/Compiler/TokenType.java
+++ Compiler/src/main/java/Compiler/TokenType.java
@@ -1,29 +1,29 @@
 package Compiler;
 
 public enum TokenType {
-	whitespaceTK(""),					//λευκός χαρακτήρας
-	newlineTK(""),						// αλλαγή γραμμής
+	whitespaceTK("\\s"),					//λευκός χαρακτήρας
+	newlineTK("\\n"),						// αλλαγή γραμμής
 	// Έκδοση 1.1 Διαγράφηκε το παρακάτω
 	// digitwordTK(""),
 	constantTK("-?\\d+\\.\\d+|-?\\d+"),	// αριθμητική σταθερά
-	programTK(""),						// δεσμευμένη λέξη program
-	declareTK(""),						// δεσμευμένη λέξη declare
-	enddeclareTK(""),					// δεσμευμένη λέξη enddeclare
+	programTK("program"),						// δεσμευμένη λέξη program
+	declareTK("declare"),						// δεσμευμένη λέξη declare
+	enddeclareTK("enddeclare"),					// δεσμευμένη λέξη enddeclare
 	beginTK("begin"),					// δεσμευμένη λέξη begin
-	endTK(""),							// δεσμευμένη λέξη end
-	printTK(""),						// δεσμευμένη λέξη print
-	inputTK(""),						// δεσμευμένη λέξη input
-	variableTK(""),						// μεταβλητή
+	endTK("end"),							// δεσμευμένη λέξη end
+	printTK("print"),						// δεσμευμένη λέξη print
+	inputTK("input"),						// δεσμευμένη λέξη input
+	variableTK("[a-zA-z]([a-ZA-z]|[0-9])*"),						// μεταβλητή
 	plusTK("\\+"),						// σύμβολο πρόσθεσης
-	minusTK(""),						// σύμβολο αφαίρεσης
-	multTK(""),							// σύμβολο πολλαπλασιασμού
-	divTK(""),							// σύμβολο διαίρεσης
-	leftpTK(""),						// σύμβολο αριστερής παρένθεσης
-	rightpTK(""),						// σύμβολο δεξιάς παρένθεσης
-	assignTK(""),						// σύμβολο εκχώρησης
-	semicolTK(""),						// ελληνικό ερωτηματικό
-	commaTK(""),						// κόμμα
-	unknownTK(""),						// άγνωστος χαρακτήρας, θα οδηγεί σε μήνυμα σφάλματος
+	minusTK("\\-"),						// σύμβολο αφαίρεσης
+	multTK("\\*"),							// σύμβολο πολλαπλασιασμού
+	divTK("\\/"),							// σύμβολο διαίρεσης
+	leftpTK("\\("),						// σύμβολο αριστερής παρένθεσης
+	rightpTK("\\)"),						// σύμβολο δεξιάς παρένθεσης
+	assignTK(":="),						// σύμβολο εκχώρησης
+	semicolTK(";"),						// ελληνικό ερωτηματικό
+	commaTK("."),						// κόμμα
+	unknownTK("."),						// άγνωστος χαρακτήρας, θα οδηγεί σε μήνυμα σφάλματος
 	ofTK("\\Z");			// τέλος προγράμματος
 
 	public final String pattern;
